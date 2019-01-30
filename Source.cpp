1d5087f153684fae18963f8283f707f62e7b8384
Mark Diorio <diorio68@rowan.edu>
2019-01-29 19:21:51 -0500

    I added the miles to kilometers source code.

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,41 @@
 
 
-/************************************************************
+
+/***********************************************
 * Name: Mark Diorio
 * Email: diorio68@students.rowan.edu
-*************************************************************
+***********************************************
 * Purpose:
-*  This program converts a distance from miles to kilometers.
-*************************************************************/
+*  This program calculates area of circle
+***********************************************/
 
 #include <iostream>
 
-using namespace std;
+using namespace std; 
+
+const  float PI = 3.14159;
 
 int main()
 {
+
+	float radius, area, circumference;
+	
+	//Display purpose of the program
+	cout << " ******************************************"<<endl;
+	cout << " This program takes in radius to compute area of circle" <<endl;
+	cout << " ******************************************\n\n";
+	
+	//Get user input
+	cout << "Please input radius: ";
+	cin >> radius;
+	
+	//Compute area to PI*radius*radius
+	area = PI* radius *radius; 
+	
+	//Display the area 
+	cout << " The area is = " << area << "\n\n";
+	cout << "Program has ended"<<endl;
+
 
 	float miles, kilometers;
 
@@ -26,12 +48,13 @@ int main()
 	cout << "Please input a distance in miles: ";
 	cin >> miles;
 	cout << "\n";
+
 	//Compute area to PI*radius*radius
 	kilometers = 1.60934 * miles;
 
 	//Display the area 
-	cout << " The distance you entered in miles was: " << miles << "\n\n";
-	cout << " The distance converted into kilometers is: " << kilometers << "\n\n";
+	cout << "The distance you entered in miles was:" << miles << "\n\n";
+	cout << "The distance converted into kilometers is: " << kilometers << "\n\n";
 	cout << "Program has ended" << endl;
 
 	system("pause");
